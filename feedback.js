97766be2122f4807eb8b0dc0c68097ef6b0d15ec
Gabizinha1002 <gabriella.marques.silva@escola.pr.gov.br>
2024-10-23 09:37:02 -0300

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -4,25 +4,17 @@ const form = document.getElementById('opiniao-form');
 form.addEventListener('submit', async function (e) {
             e.preventDefault();
 
-            const opiniao = document.getElementById('opiniao').value;
-            try {
-                const response = await fetch('https://api.sheetmonkey.io/form/xfv8Kx1ihg6eHYj5tnAFop', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        opiniao: opiniao
-                    }),
-                });
+         const formData = {
+        opiniao: document.getElementById('opiniao').value,
+        pergunta: document.getElementById('pergunta').value,
+    };
 
-                if (response.ok) {
-                    document.getElementById('mensagem-sucesso').style.display = 'block';
-                    form.reset();
-                } else {
-                    alert('Ocorreu um erro ao enviar sua opinião.');
-                }
-            } catch (error) {
-                alert('Erro de conexão, tente novamente.');
-            }
-        });
+    fetch('https://api.sheetmonkey.io/form/ig9CUs7w52dHRERb9qMuo3', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+});
+
